0be414619c2fda6e841e4409a8e32492d89c472d
jlw2023 <12750395@me.mcu.edu.tw>
2024-03-13 12:02:04 +0800

    week01

--- week01-1.py
+++
@@ -1,6 +0,0 @@
-a,b,c = list(map(int, input() .split()))
-.....
-a,b,c = list(map(int, input() .split()))
-if a>=b and a>=c: print(a)
-elif b>=a and a>=c: print(b)
-else: print(c)
---
+++ week01/week01-2.py
@@ -0,0 +1,6 @@
+a,b,c = list(map(int, input() .split()))
+.....
+a,b,c = list(map(int, input() .split()))
+if a>=b and a>=c: print(a)
+elif b>=a and a>=c: print(b)
+else: print(c)
