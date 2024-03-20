390c89bf01e986669bdf71247231043747e82cd9
jlw2023 <12750395@me.mcu.edu.tw>
2024-03-20 12:11:37 +0800

    week05

---
+++ week05/week05-3.py
@@ -0,0 +1,8 @@
+for i in range(9):#0....8(小於9),共9個
+  print('i是',i, end=' ')
+  print()#換行
+for i in range(1,9+1):
+  print('i是',i, end=' ')
+print() #跳行
+for i in range(9-1,-1,-1):
+  print('i是',i, end=' ')
